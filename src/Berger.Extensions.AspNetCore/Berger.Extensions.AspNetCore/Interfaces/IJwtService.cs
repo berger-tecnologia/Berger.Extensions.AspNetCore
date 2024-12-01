aab1019e0d47aa9b1934ef851641b544c671722c
Rafael Cronemberger <rafael@berger.com.br>
2024-12-01 14:24:45 -0300

    .NET 9 Migration

---
+++ src/Berger.Extensions.AspNetCore/Berger.Extensions.AspNetCore/Interfaces/IJwtService.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+
+namespace Berger.Extensions.AspNetCore
+{
+    public interface IJwtService
+    {
+        Token Issue();
+        string Issue(List<Claim> claims, JwtConfig config);
+        string RefreshToken(string expiredToken, string refreshToken, JwtConfig config);
+    }
+}
